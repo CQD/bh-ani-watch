48555301f6fc6a2961258c5413f767d6c384968e
CQD <cqd@hiigara.net>
2019-07-14 18:49:03 +0800

    實作 anime 紀錄邏輯與取資料的 api

--- public/index.php
+++ public/index.php
@@ -1,4 +1,49 @@
 <?php
+require __DIR__ . '/../vendor/autoload.php';
 
-$a = getenv('DATASTORE_EMULATOR_HOST');
-var_dump($a);
+use MinorWork\App;
+
+mb_internal_encoding('UTF-8');
+date_default_timezone_set('Asia/Taipei');
+
+setupConstants();
+
+$app = new App;
+$app->handlerClassPrefix = '\Q\BHAniWatch\Controller\\';
+
+$app->handlerAlias([
+    'cache' => function($app){
+        $etag = md5((string) $app->view);
+        header("ETag: {$etag}");
+        header("Cache-Control: public, max-age=86400");
+    },
+]);
+
+$app->setRouting([
+    'animeInfo'     => ['/api/anime/{id:[\d]+}',      ['Anime:info', 'cache']],
+    'animeIdList'   => ['/api/anime/allId',           ['Anime:listId', 'cache']],
+    'jobAnimeList'  => ['/_job/fetchAnimeList',       'Anime:fetchList'],
+]);
+$app->run();
+
+
+exit;
+/////////////////////////////////////////////
+
+function setupConstants()
+{
+    if (!defined('GAE_APPLICATION')) {
+        define('GAE_APPLICATION', call_user_func(function(){
+            // App Engine 輸入的 Application ID 前面會帶 prefix
+            // "s~"   代表正式環境
+            // "dev~" 代表開發環境
+            // 使用時要把 prefix 拔掉才是真正的 application id
+            // https://stackoverflow.com/a/5901750
+            $appId = $_SERVER['APPLICATION_ID'] ?? 'bh-ani-watch';
+            return preg_replace('/^[^~]*~/', '', $appId);
+        }));
+    }
+
+    $ver = defined('GAE_VERSION') ? GAE_VERSION : '201907141706';
+    define('ASSET_VERSION', abs(crc32($ver . 'salt')));
+}
---
+++ src/Controller/Anime.php
@@ -0,0 +1,138 @@
+<?php
+
+namespace Q\BHAniWatch\Controller;
+
+use Google\Cloud\Datastore\DatastoreClient;
+use Google\Cloud\Datastore\Query\Query;
+use Q\BHAniWatch\Helper\Api;
+
+class Anime
+{
+    public function info($app, $params, $prevOutput)
+    {
+        $store = $this->getStore();
+        $key = $store->key('anime', $params['id']);
+        $anime = $store->lookup($key);
+
+        $data = ($anime)
+            ? $anime->get()
+            : null;
+
+        header('Content-Type: application/json');
+        $app->view = json_encode($data);
+    }
+
+    public function listId($app, $params, $prevOutput)
+    {
+        $store = $this->getStore();
+        $allIds = [];
+
+        $cursor = '';
+        while (true) {
+            $query = $store->query()
+                ->kind('anime')
+                ->keysOnly()
+                ->order('order', Query::ORDER_DESCENDING)
+                ->start($cursor);
+
+            $res = $store->runQuery($query);
+
+            $cursor = false;
+            foreach ($res as $anime) {
+                $allIds[] = $anime->key()->pathEndIdentifier();
+                $cursor = $anime->cursor();
+            }
+
+            if (!$cursor) break;
+        }
+
+        header('Content-Type: application/json');
+        $app->view = json_encode($allIds);
+    }
+
+    public function fetchList($app, $params, $prevOutput)
+    {
+        $api = new Api;
+
+        $data = [];
+        $page = 1;
+        while ($result = $api->call('/v1/list.php', ['page' => $page])) {
+            foreach ($result as $row) {
+                $data[] = $row;
+            }
+            $page++;
+            usleep(200 * 1000);
+        }
+
+        $this->saveAnime($data);
+        $this->saveAnimeDailyScore($data);
+        $app->view = 'done';
+    }
+
+    private function saveAnime($data)
+    {
+        $store = $this->getStore();
+
+        $step = 30;
+        $order = count($data);
+
+        for ($offset = 0; $offset < count($data); $offset += $step) {
+            $chunk = array_slice($data, $offset, $step);
+            $rows = [];
+
+            foreach ($chunk as $anime) {
+                $key = $store->key('anime', $anime['anime_sn']);
+                $row = [
+                    'anime_sn' => (int) $anime['anime_sn'] ?? 0,
+                    'title' => (string) $anime['title'] ?? '不明',
+                    'info' => (string) $anime['info'] ?? '不明',
+                    'popular' => (int) $anime['popular'] ?? 0,
+                    'score' => (double) $anime['score'] ?? 0,
+                    'order' => (int) ($order--),
+                ];
+                $row = $store->entity($key, $row, [
+                    'excludeFromIndexes' => ['info', 'title'],
+                ]);
+                $rows[] = $row;
+            }
+
+            $store->upsertBatch($rows);
+        }
+    }
+
+    private function saveAnimeDailyScore($data)
+    {
+        $ymd = date('Ymd');
+        $store = $this->getStore();
+
+        $score = [];
+        $popular = [];
+        foreach ($data as $anime) {
+            $animeSn = (int) $anime['anime_sn'] ?? 0;
+            $popular[$animeSn] = (int) $anime['popular'];
+            $score[$animeSn] = (int) $anime['score'];
+        }
+
+        $key = $store->key('anime_daily_score', $ymd);
+
+        $entity = [
+            'date' => (int) $ymd,
+            'popular' => json_encode($popular),
+            'score' => json_encode($score),
+        ];
+        $entity = $store->entity($key, $entity, [
+            'excludeFromIndexes' => ['popular', 'score'],
+        ]);
+        $store->upsert($entity);
+    }
+
+    private function getStore()
+    {
+        static $store = null;
+        $store = $store ?: $datastore = new DatastoreClient([
+            'projectId' => GAE_APPLICATION,
+        ]);
+
+        return $store;
+    }
+}
